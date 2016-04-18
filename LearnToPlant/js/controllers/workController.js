ca3627637d14c06942ed235c0eee372927ef05a9
rafitko <rafal.makowski@yahoo.com>
2016-04-19 01:03:03 +0200

    3 Arrows Tutorial

--- LearnToPlant/js/controllers/workController.js
+++ LearnToPlant/js/controllers/workController.js
@@ -4,15 +4,21 @@
     var controllerId = 'workController';
 
     angular.module('app').controller(controllerId,
-        ['$scope', '$rootScope', 'workFactory', workController]);
+        ['$scope', '$rootScope', 'workFactory', 'imageFactory', workController]);
 
-    function workController($scope, $rootScope, workFactory) {
+    function workController($scope, $rootScope, workFactory, imageFactory) {
         $rootScope.plant = {};
         $rootScope.level = 1;
-        
+
         workFactory.getWorkArea($rootScope.level).success(function (data) {
             $rootScope.plant = data;
             console.log($rootScope.plant);
+        }).error(function (error) {
+            // log errors
+        });
+
+        imageFactory.getImages(1, "work").success(function (data) {
+            $scope.images = data;
         }).error(function (error) {
             // log errors
         });
--- LearnToPlant/js/pop-up.js
+++ LearnToPlant/js/pop-up.js
@@ -1,6 +1,6 @@
 ﻿// jQuery pop-up div
 $(function () {
-    $('div.popup').hide();
+    $('.popup').hide();
 
     Krok0();
 
@@ -22,36 +22,36 @@ function Krok0() {
 
 function Krok1() {
     setTimeout(function () {
-        $('#welcome div.lewo').fadeIn("slow");
+        $('#welcome .lewo').fadeIn("slow");
         $('#tutorial').text("Lewy panel służy do wyboru lekcji!");
     }, 5000);
 }
 
 function Krok2() {
     setTimeout(function () {
-        $('#welcome div.lewo').fadeOut("slow");
-        $('#welcome div.srodek').fadeIn("slow");
+        $('#welcome .lewo').hide();
+        $('#welcome .srodek').fadeIn("slow");
         $('#tutorial').text("Środkowy panel przedstawia Twoją roślinkę!");
     }, 10000);
 }
 
 function Krok3() {
     setTimeout(function () {
-        $('#welcome div.srodek').fadeOut("slow");
-        $('#welcome div.prawo').fadeIn("slow");
+        $('#welcome .srodek').hide();
+        $('#welcome .prawo').fadeIn("slow");
         $('#tutorial').text("Z prawej strony znajdują się wszystkie Twoje narzędzia!");
     }, 15000);
 }
 
 function Krok4() {
     setTimeout(function () {
-        $('#welcome div.prawo').fadeOut("slow");
-        $('#welcome div.srodek').fadeIn("slow");
+        $('#welcome .prawo').hide();
+        $('#welcome .srodek').fadeIn("slow");
         $('#tutorial').text("Powodzenia!");
     }, 20000);
 
     setTimeout(function () {
-        $('#welcome div.srodek').fadeOut("slow");
+        $('#welcome .srodek').fadeOut("slow");
         $('#welcome').hide();
     }, 28000);
 }
