98f6159288b875396459edf17ba8c5b6e1d4d3f1
rafitko <rafal.makowski@yahoo.com>
2016-05-24 16:43:20 +0200

    Simple sub images example

--- LearnToPlant/js/controllers/toolsController.js
+++ LearnToPlant/js/controllers/toolsController.js
@@ -8,9 +8,19 @@
 
     function toolsController($scope, $rootScope, imageFactory) {
         $scope.images = [];
+        $scope.subimages = [];
+
+        $scope.hoverIn = function () {
+            this.hoverEdit = true;
+        };
+
+        $scope.hoverOut = function () {
+            this.hoverEdit = false;
+        };
 
         imageFactory.getImages($rootScope.level, "tools").success(function (data) {
             $scope.images = data;
+            $scope.subimages = data.SubImages;
             console.log(data);
         }).error(function (error) {
             // log errors
